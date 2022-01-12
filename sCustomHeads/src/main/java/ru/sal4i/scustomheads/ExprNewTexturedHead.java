652d46b2257b8f21cf180ab25eb0d90f0d002bfa
Sal4iDev <96550478+Sal4iDev@users.noreply.github.com>
2022-01-12 23:51:29 +0300

    refactoring

--- sCustomHeads/src/main/java/ru/sal4i/scustomheads/ExprNewTexturedHead.java
+++ sCustomHeads/src/main/java/ru/sal4i/scustomheads/ExprNewTexturedHead.java
@@ -7,6 +7,37 @@ import com.gmail.visualbukkit.project.BuildContext;
 
 @SuppressWarnings({"unused", "TextBlockMigration"})
 public class ExprNewTexturedHead extends Expression {
+    private static final String METHOD =
+            """
+                    public static org.bukkit.inventory.ItemStack getSkull(String texture, String name, java.util.List<String> lore) {
+                            org.bukkit.inventory.ItemStack head = new org.bukkit.inventory.ItemStack(org.bukkit.Material.PLAYER_HEAD);
+                            org.bukkit.inventory.meta.SkullMeta headMeta = (org.bukkit.inventory.meta.SkullMeta) head.getItemMeta();
+                            headMeta.setLore(lore);
+                            
+                            if (texture == null || texture.isEmpty()) {
+                                headMeta.setDisplayName(name);
+                                head.setItemMeta(headMeta);
+                                return head;
+                            }
+                                            
+                            com.mojang.authlib.GameProfile profile = new com.mojang.authlib.GameProfile(java.util.UUID.randomUUID(), null);
+                            profile.getProperties().put("textures", new com.mojang.authlib.properties.Property("textures", texture));
+                            java.lang.reflect.Field profileField;
+                            try {
+                                profileField = headMeta.getClass().getDeclaredField("profile");
+                                profileField.setAccessible(true);
+                                profileField.set(headMeta, profile);
+                            } catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException exception) {
+                                exception.printStackTrace();
+                            }
+                            
+                            headMeta.setDisplayName(name);
+                            headMeta.setLore(lore);
+                            head.setItemMeta(headMeta);
+                            return head;
+                        }
+                        """;
+
     public ExprNewTexturedHead() {
         super(
                 "expr-new-custom-head-with-name-and-lore",
@@ -48,35 +79,4 @@ public class ExprNewTexturedHead extends Expression {
 
         };
     }
-
-    private static final String METHOD =
-            """
-                    public static org.bukkit.inventory.ItemStack getSkull(String texture, String name, java.util.List<String> lore) {
-                            org.bukkit.inventory.ItemStack head = new org.bukkit.inventory.ItemStack(org.bukkit.Material.PLAYER_HEAD);
-                            org.bukkit.inventory.meta.SkullMeta headMeta = (org.bukkit.inventory.meta.SkullMeta) head.getItemMeta();
-                            headMeta.setLore(lore);
-                            
-                            if (texture == null || texture.isEmpty()) {
-                                headMeta.setDisplayName(name);
-                                head.setItemMeta(headMeta);
-                                return head;
-                            }
-                                            
-                            com.mojang.authlib.GameProfile profile = new com.mojang.authlib.GameProfile(java.util.UUID.randomUUID(), null);
-                            profile.getProperties().put("textures", new com.mojang.authlib.properties.Property("textures", texture));
-                            java.lang.reflect.Field profileField;
-                            try {
-                                profileField = headMeta.getClass().getDeclaredField("profile");
-                                profileField.setAccessible(true);
-                                profileField.set(headMeta, profile);
-                            } catch (NoSuchFieldException | IllegalArgumentException | IllegalAccessException exception) {
-                                exception.printStackTrace();
-                            }
-                            
-                            headMeta.setDisplayName(name);
-                            headMeta.setLore(lore);
-                            head.setItemMeta(headMeta);
-                            return head;
-                        }
-                        """;
 }
