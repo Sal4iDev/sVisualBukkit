9997c9b49ac016fa389696b56104b2e949b30dbd
Sal4iDev <96550478+Sal4iDev@users.noreply.github.com>
2022-01-20 19:52:16 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,9 @@
 rootProject.name = "sVisualBukkit-Projects"
+// Public extensions
 include("sCustomHeads")
 include("sUpdateChecker")
 include("sTabCompletion")
+
+// Private extensions (ordered)
 include("sServerControl")
 include("sFileDownloader")
