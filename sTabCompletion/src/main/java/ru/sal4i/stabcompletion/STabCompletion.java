9b674280df4f84d4c8907e0da3e00cb43019d81b
Sal4iDev <96550478+Sal4iDev@users.noreply.github.com>
2022-01-21 13:23:48 +0300

    tests

--- sTabCompletion/src/main/java/ru/sal4i/stabcompletion/STabCompletion.java
+++ sTabCompletion/src/main/java/ru/sal4i/stabcompletion/STabCompletion.java
@@ -18,6 +18,8 @@ public class STabCompletion extends VisualBukkitExtension {
 
         List.of(" ", "Order plugins and extensions from Sal4iDev",
                 "Discord: Sal4iDev#4767", "Have a nice day <3!", " ").forEach(System.out::println);
+
+//        Platform.runLater(this::checkUpdates);
     }
 
     @Override
@@ -39,4 +41,26 @@ public class STabCompletion extends VisualBukkitExtension {
     public String getDescription() {
         return "Adds the ability to make a simple tab completion for your command";
     }
+//
+//    private void checkUpdates() {
+//        try (InputStream inputStream = new URL("https://api.github.com/repos/Sal4iDev/sVisualBukkit/releases/latest").openStream()) {
+//            JSONObject response = new JSONObject(new JSONTokener(inputStream));
+//            String latestVersion = response.getString("tag_name");
+//            if (getVersion().equals(latestVersion)) return;
+//            ButtonType viewButton = new ButtonType("View Update");
+//            Alert alert = new Alert(Alert.AlertType.INFORMATION, String.format("An update is " +
+//                    "available!\nUsing version: %s\nLatest version: %s", getVersion(), latestVersion),
+//                    viewButton, new ButtonType("Ignore Update"));
+//            VisualBukkitApp.getSettingsManager().style(alert.getDialogPane());
+//            alert.setTitle(getName() + " - Update Available!");
+//            alert.setGraphic(null);
+//            alert.setHeaderText(null);
+//            alert.showAndWait().ifPresent(buttonType -> {
+//                if (buttonType == viewButton) {
+//                    VisualBukkitApp.openURI(URI.create("https://github.com/Sal4iDev/sVisualBukkit/releases"));
+//                }
+//            });
+//        } catch (Exception ignored) {
+//        }
+//    }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,5 +5,6 @@ include("sUpdateChecker")
 include("sTabCompletion")
 
 // Private extensions (custom)
-include("sServerControl")
-include("sFileDownloader")
+include("sServerControl") // 8$
+include("sFileDownloader") // 8$
+include("sDiscordWebhooks") // 15$
